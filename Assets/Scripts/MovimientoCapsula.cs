80dec7cf154c10ee927ce3f513796a9ee738201f
poboma <polboladeres@gmail.com>
2025-12-08 12:06:35 +0100

    estandarizacion

--- Assets/Scripts/MovimientoCapsula.cs
+++ Assets/Scripts/MovimientoCapsula.cs
@@ -7,10 +7,10 @@ public class PlayerMovementSimple : MonoBehaviour
     [SerializeField] public float movementSpeed = 5f;
     [SerializeField] public float turnSpeed = 90f;
     public Transform ubicacionInicial;
-    private CharacterController player;
+    private CharacterController jugador;
     private void Start()
     {
-        player = GetComponent<CharacterController>();
+        jugador = GetComponent<CharacterController>();
     }
     void Update()
     {
@@ -24,15 +24,15 @@ public class PlayerMovementSimple : MonoBehaviour
         transform.Rotate(0f, turn * turnSpeed * Time.deltaTime, 0f);
         Vector3 localMovement = new Vector3(0, 0, travel * movementSpeed);
         Vector3 worldMovement = transform.TransformDirection(localMovement);
-        player.Move(worldMovement * Time.deltaTime);
+        jugador.Move(worldMovement * Time.deltaTime);
     }
 
     private void resetLaberinth()
     {
-        player.enabled = false;
+        jugador.enabled = false;
         transform.position = ubicacionInicial.position;
         transform.forward = ubicacionInicial.forward;
-        player.enabled = true;
+        jugador.enabled = true;
 
       
     }
